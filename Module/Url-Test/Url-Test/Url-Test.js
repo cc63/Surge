df028c5b0357a455d3137dede8277e9c66b198b8
Uex <99179360+xbs8@users.noreply.github.com>
2023-06-17 13:56:33 +0800

    Update Url-Test.js

--- Module/Url-Test/Url-Test/Url-Test.js
+++ Module/Url-Test/Url-Test/Url-Test.js
@@ -23,7 +23,7 @@ function http(req) {
 			let time = Date.now();
         $httpClient.post($[req], (err, resp, data) => {
             r(req +
-'\xa0\t:' +
+'\xa0: ' +
 (Date.now() - time)+' ms');
         });
     });
