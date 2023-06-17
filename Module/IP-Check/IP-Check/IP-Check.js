abb25a4ff0a91a744bde4fdfb2cb4817541fa0b1
Uex <99179360+xbs8@users.noreply.github.com>
2023-06-17 13:29:28 +0800

    Update IP-Check.js

--- Module/IP-Check/IP-Check/IP-Check.js
+++ Module/IP-Check/IP-Check/IP-Check.js
@@ -17,8 +17,8 @@ $httpClient.get(url, function(error, response, data){
     let ip = jsonData.query
   body = {
     title: "节点信息",
-    content: `IP信息：${ip}\n运营商：${isp}\n所在地：${emoji}${country} - ${city}`,
-    icon: "globe.asia.australia.fill"
+    content: `节点IP：${ip}\n节点ISP：${isp}\n节点位置：${emoji} │ ${country} - ${city}`,
+    icon: "globe.asia.australia"
   }
   $done(body);
 });
