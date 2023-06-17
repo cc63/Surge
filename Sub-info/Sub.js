3670ea2a1c82a5ba51b160b7aba461ab0f5b2c67
Uex <99179360+xbs8@users.noreply.github.com>
2023-06-17 11:06:44 +0800

    Update Sub.js

--- Sub-info/Sub.js
+++ Sub-info/Sub.js
@@ -14,7 +14,7 @@ let args = getArgs();
   let used = info.download + info.upload;
   let total = info.total;
   let expire = args.expire || info.expire;
-  let content = [`用量：${bytesToSize(used)}/${bytesToSize(total)}`];
+  let content = [`用量：${bytesToSize(used)} | ${bytesToSize(total)}`];
 
   if (resetDayLeft) {
     content.push(`重置：剩余${resetDayLeft}天`);
