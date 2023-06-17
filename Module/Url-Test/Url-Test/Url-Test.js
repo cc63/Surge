81f1ddda817265a6ee6481af9086ea7b4e631209
Uex <99179360+xbs8@users.noreply.github.com>
2023-06-17 13:45:05 +0800

    Create Url-Test.js

---
+++ Module/Url-Test/Url-Test/Url-Test.js
@@ -0,0 +1,31 @@
+/*
+感谢@小白脸 重写脚本原脚本
+原作者@yibeizipeini来自于https://raw.githubusercontent.com/yibeizipeini/JavaScript/Surge/ConnectivityTest.js
+*/
+let $ = {
+Baidu:'https://www.baidu.com',
+Google:'https://www.google.com/generate_204',
+Github:'https://www.github.com'
+}
+
+!(async () => {
+await Promise.all([http('Baidu'),http('Google'),http('Github')]).then((x)=>{
+	$done({
+    title: '连通性测试',
+    content: x.join('\n'),
+    icon: 'personalhotspot',
+    'icon-color': '#0DCB27',
+  })
+})
+})();
+
+function http(req) {
+    return new Promise((r) => {
+			let time = Date.now();
+        $httpClient.post($[req], (err, resp, data) => {
+            r(req +
+						'\xa0\xa0\xa0\t: ' +
+						(Date.now() - time)+' ms');
+        });
+    });
+}
