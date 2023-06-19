c2229bd7814bd28e7c1f5d85a8348f36b81cb618
Uex <99179360+xbs8@users.noreply.github.com>
2023-06-19 11:28:48 +0800

    更新 yj.js

--- Module/Youjia/yj.js
+++ Module/Youjia/yj.js
@@ -95,7 +95,7 @@ $httpClient.get(
             else {
                 body = {
                     title: "实时油价信息",
-                    content: `${prices[0].name}  ${prices[0].value}\n${prices[1].name}  ${prices[1].value}\n${prices[2].name}  ${prices[2].value}\n---------------------------\n${friendly_tips}`,
+                    content: `${prices[0].name}  ${prices[0].value}\n${prices[1].name}  ${prices[1].value}\n${prices[2].name}  ${prices[2].value}\n${friendly_tips}`,
                     icon: "fuelpump.fill"
                                }
 
