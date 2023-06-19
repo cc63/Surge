6045d61ff09bd7f2108b3f46783ae64e4dc9c100
Uex <99179360+xbs8@users.noreply.github.com>
2023-06-19 11:36:21 +0800

    更新 yj.js

--- Module/Youjia/yj.js
+++ Module/Youjia/yj.js
@@ -56,45 +56,13 @@ $httpClient.get(
                 })
             }
 
-            // 解析油价调整趋势
-            var adjust_date = ''
-            var adjust_trend = ''
-            var adjust_value = ''
-
-            const reg_adjust_tips = /<div class="tishi"> <span>(.*)<\/span><br\/>([\s\S]+?)<br\/>/
-            const adjust_tips_match = data.match(reg_adjust_tips)
-
-            if (adjust_tips_match && adjust_tips_match.length === 3) {
-                adjust_date = adjust_tips_match[1].split('价')[1].slice(0, -2)
-
-                adjust_value = adjust_tips_match[2]
-                adjust_trend = (adjust_value.indexOf('下调') > -1 || adjust_value.indexOf('下跌') > -1) ? '下跌' : '上涨'
-
-                const adjust_value_re = /([\d\.]+)元\/升-([\d\.]+)元\/升/
-                const adjust_value_re2 = /[\d\.]+元\/吨/
-                const adjust_value_match = adjust_value.match(adjust_value_re)
-
-                if (adjust_value_match && adjust_value_match.length === 3) {
-                    adjust_value = `${adjust_value_match[1]}-${adjust_value_match[2]}元/升`
-                }
-                else {
-                    const adjust_value_match2 = adjust_value.match(adjust_value_re2)
-
-                    if (adjust_value_match2) {
-                        adjust_value = adjust_value_match2[0]
-                    }
-                }
-            }
-
-            const friendly_tips = `${adjust_date} ${adjust_trend} ${adjust_value}`
-
             if (prices.length !== 4) {
                 console.log(`解析油价信息失败, 数量=${prices.length}, 请反馈至 @RS0485: URL=${query_addr}`)
                 done({})
             }
             else {
                 body = {
-                    title: "${friendly_tips}",
+                    title: "实时油价信息",
                     content: `${prices[0].name}  ${prices[0].value}\n${prices[1].name}  ${prices[1].value}\n${prices[2].name}  ${prices[2].value}`,
                     icon: "fuelpump.fill"
                                }
