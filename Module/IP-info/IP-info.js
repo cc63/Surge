941561bd95ee18a7590ad7f4280b8c2d2ef348b1
Uex <99179360+cc63@users.noreply.github.com>
2023-07-16 17:06:12 +0800

    Update IP-info.js

--- Module/IP-info/IP-info.js
+++ Module/IP-info/IP-info.js
@@ -13,7 +13,7 @@ $httpClient.get(url, function(error, response, data){
     body = {
         title: "节点信息",
         content: `IP地址：${ip}\n运营商：${isp}\n所在地：${location}`,
-        icon: "network"
+        icon: "globe.asia.australia"
     }
     $done(body);
 });
