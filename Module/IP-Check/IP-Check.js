c10f2727d1c9e480db3789ff829a489bbe27668d
Uex <99179360+xbs8@users.noreply.github.com>
2023-07-16 14:43:41 +0800

    更新 IP-Check.js

--- Module/IP-Check/IP-Check.js
+++ Module/IP-Check/IP-Check.js
@@ -7,22 +7,24 @@ $httpClient.get(url, function(error, response, data){
     let city = jsonData.city
     let isp = jsonData.isp
     let ip = jsonData.query
-  body = {
-    title: "节点信息",
-    content: `节点IP：${ip}\n运营商：${isp}\n所在地：${emoji} │ ${country}`,
-    icon: "globe.asia.australia"
-  }
-  $done(body);
+
+    let location = (country === city) ? `${emoji} │ ${country}` : `${emoji} │ ${country}-${city}`;
+
+    body = {
+        title: "节点信息",
+        content: `节点IP：${ip}\n运营商：${isp}\n所在地：${location}`,
+        icon: "globe.asia.australia"
+    }
+    $done(body);
 });
 
 function getFlagEmoji(countryCode) {
-      if (countryCode.toUpperCase() == 'TW') {
-    countryCode = 'CN'
-  }
-  const codePoints = countryCode
-    .toUpperCase()
-    .split('')
-    .map(char => 127397 + char.charCodeAt())
-  return String.fromCodePoint(...codePoints)
-
+    if (countryCode.toUpperCase() == 'TW') {
+        countryCode = 'CN'
+    }
+    const codePoints = countryCode
+        .toUpperCase()
+        .split('')
+        .map(char => 127397 + char.charCodeAt())
+    return String.fromCodePoint(...codePoints)
 }
