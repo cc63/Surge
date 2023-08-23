983797f0efe5dd1e82b38a7dfd2ecfd7d0529dd8
Uex <99179360+cc63@users.noreply.github.com>
2023-08-23 11:06:53 +0800

    Update CN-IP.js

--- Module/CN-IP/CN-IP.js
+++ Module/CN-IP/CN-IP.js
@@ -4,13 +4,12 @@ $httpClient.get(url, function(error, response, data){
     let cnjsonData = JSON.parse(data)
     let cnemoji = getFlagEmoji(jsonData.countryCode)
     let cnprovince = jsonData.province
-    let cncity = jsonData.city
     let cnisp = jsonData.isp
     let cnip = jsonData.addr
     
     body = {
         title: "网络信息",
-        content: `IP地址：${cnip}\n运营商：${emoji} │ ${cnprovince}${cnisp}`,
+        content: `IP地址：${cnip}\n运营商：${cnemoji} │ ${cnprovince}${cnisp}`,
         icon: "personalhotspot"
     }
     $done(body);
