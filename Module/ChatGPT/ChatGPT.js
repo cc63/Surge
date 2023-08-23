611e3a0b90978938b8ddbc4b0047f6e1eeef7fd7
Uex <99179360+cc63@users.noreply.github.com>
2023-08-23 12:13:49 +0800

    Update ChatGPT.js

--- Module/ChatGPT/ChatGPT.js
+++ Module/ChatGPT/ChatGPT.js
@@ -55,7 +55,7 @@ $httpClient.get(url, function(error, response, data) {
   
   let flagEmoji = getFlagEmoji(loc, flagMapping);
   
-  let content = `${gpt} , 区域: ${loc}`;
+  let content = `${gpt} , ${loc}地区`;
   
   let body = {
     title: "ChatGPT",
