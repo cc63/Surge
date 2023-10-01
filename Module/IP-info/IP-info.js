16335c3a7745c21101d64a770c77f1540a3838a8
Uex <99179360+cc63@users.noreply.github.com>
2023-10-01 06:10:22 -0500

    更新 IP-info.js

--- Module/IP-info/IP-info.js
+++ Module/IP-info/IP-info.js
@@ -11,7 +11,7 @@ $httpClient.get(url, function(error, response, data){
 //（暂时舍弃，改用不带City的）let location = (country === city) ? `${emoji} │ ${country}` : `${emoji} │ ${country}-${city}`;
 let location = (country === city) ? `${emoji} │ ${country}` : `${emoji} │ ${country}`;
 // 去除 isp 变量中的标点符号和 "Communications" 等过长的无意义词语
-let cleanedIsp = isp.replace(/[,]|\s(\.$)|\sCommunications|\sInformation|\sTechnology|\sTechnologies|\sTelevision|\sRegistration/g, '');
+let cleanedIsp = isp.replace(/[,]|(\.$)|\sCommunications|\sInformation|\sTechnology|\sTechnologies|\sTelevision|\sRegistration/g, '');
 
 // 然后将 cleanedIsp 用于通知内容
 let body = {
