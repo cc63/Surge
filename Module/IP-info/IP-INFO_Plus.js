05b51c0782b94f54cb434b6305f1b67b14042973
Uex <99179360+cc63@users.noreply.github.com>
2023-10-14 01:41:33 +0800

    更新 IP-INFO_Plus.js

--- Module/IP-info/IP-INFO_Plus.js
+++ Module/IP-info/IP-INFO_Plus.js
@@ -11,10 +11,10 @@ $httpClient.get(url, function(error, response, data){
     let location = (country === city) ? `${emoji} │ ${country}` : `${emoji} │ ${country}`;
 
     // 获取入口 IP 信息
-    let entryIp = $request.headers["X-Real-IP"] || $request.headers["x-real-ip"] || $request.headers["X-Forwarded-For"];
+    let entryIp = entryIPAddress();
 
     // 获取入口运营商信息
-    let entryIsp = $request.headers["X-ISP"] || $request.headers["x-isp"];
+    let entryIsp = entryISP();
 
     // 去除 isp 变量中的标点符号和 "Communications" 等过长的无意义词语(不区分大小写）
     let cleanedIsp = isp.replace(/[,]|(\.$)|\([^)]*\)|Communications|Information|Technology|Technologies|Television|Registration/gi, '');
@@ -41,4 +41,17 @@ function getFlagEmoji(countryCode) {
         .split('')
         .map(char => 127397 + char.charCodeAt())
     return String.fromCodePoint(...codePoints)
+}
+
+function entryIPAddress() {
+    // 在此函数中编写获取入口 IP 的代码
+    // 您可以使用各种方法，如请求头、代理服务器等来获取入口 IP
+    // 返回入口 IP 的字符串
+    // 例如：return $request.headers["X-Real-IP"] || $request.headers["x-real-ip"];
+}
+
+function entryISP() {
+    // 在此函数中编写获取入口 IP 运营商的代码
+    // 您可以使用各种方法，如请求头、代理服务器等来获取入口 IP 运营商信息
+    // 返回入口 IP 运营商信息的字符串
 }
