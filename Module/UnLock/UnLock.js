ac6ea163d0a8dafd26e31115ca1b59f7b6b1f8d5
Uex <99179360+cc63@users.noreply.github.com>
2023-10-14 20:03:19 +0800

    Update UnLock.js

--- Module/UnLock/UnLock.js
+++ Module/UnLock/UnLock.js
@@ -25,7 +25,6 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
       icon: 'play.tv.fill',
       'icon-color': '#FF2D55',
     }
-  let [{ region, status }] = 
   let [{ region, status }] = await Promise.all([testDisneyPlus()])
     await Promise.all([check_youtube_premium(),check_netflix()])
         .then((result) => { 
