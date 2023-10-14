68bd5f9f1aac0e403ca11012f5133d03f31653e7
Uex <99179360+cc63@users.noreply.github.com>
2023-10-14 19:24:19 +0800

    Update UnLock.js

--- Module/UnLock/UnLock.js
+++ Module/UnLock/UnLock.js
@@ -32,17 +32,17 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
  let disney_result=""
     if (status==STATUS_COMING) {
         //console.log(1)
-        disney_result="Disney即将登陆 ➟ "+region.toUpperCase()
+        disney_result="Disney 即将登陆 ➟ "+region.toUpperCase()
       } else if (status==STATUS_AVAILABLE){
         //console.log(2)
         console.log(region)
-        disney_result="Disney已解锁 ➟ "+region.toUpperCase()
+        disney_result="Disney 已解锁 ➟ "+region.toUpperCase()
         // console.log(result["Disney"])
       } else if (status==STATUS_NOT_AVAILABLE) {
         //console.log(3)
-        disney_result="Disney未解锁 ➟ 🚫失败 "
+        disney_result="Disney 未解锁 ➟ 🚫失败 "
       } else if (status==STATUS_TIMEOUT) {
-        disney_result="Disney未解锁 ➟ 🚦超时 "
+        disney_result="Disney 未解锁 ➟ 🚦超时 "
       }
 result.push(disney_result)
 console.log(result)
@@ -88,7 +88,7 @@ panel_result['content'] = content
       })
     }
   
-    let youtube_check_result = 'YouTube'
+    let youtube_check_result = 'YouTube '
   
     await inner_check()
       .then((code) => {
@@ -144,7 +144,7 @@ panel_result['content'] = content
       })
     }
   
-    let netflix_check_result = 'Netflix'
+    let netflix_check_result = 'Netflix '
   
     await inner_check(80062035)
       .then((code) => {
