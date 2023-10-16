c3f8811368eb867c95b61d2d51916bde7ea81821
Uex <99179360+cc63@users.noreply.github.com>
2023-10-16 13:20:51 +0800

    更新 IP-info.js

--- Module/IP-info/IP-info.js
+++ Module/IP-info/IP-info.js
@@ -11,7 +11,7 @@ $httpClient.get(url, function(error, response, data){
 // 避免City与Country重复出现
 let location = (country === city) ? `${emoji} │ ${country}` : `${emoji}${countryCode} │ ${city}`;
 // 去除 isp 变量中的标点符号和 "Communications" 等过长的无意义词语(不区分大小写）
-let cleanedIsp = isp.replace(/[,]|(\.$)|\([^)]*\)|Communications|Information|Technology|Technologies|Television|Registration/gi, '');
+let cleanedIsp = isp.replace(/[,]|(\.$)|\([^)]*\)|\-|Communications|Telekomunikasyon|Hosted|Information|Technology|Technologies|Television|Registration/gi, '');
 // 将运营商信息中的连续两个或多个空格替换为一个空格
     cleanedIsp = cleanedIsp.replace(/ {2,}/g, ' ');
 // 然后将 cleanedIsp 用于通知内容
