ff884aae2c3364e34e4b7d3fcc91824aaf75a849
C63 <99179360+cc63@users.noreply.github.com>
2023-11-06 22:55:36 +0800

    更新 IP-info.js

--- Module/Panel/IP-info/Moore/IP-info.js
+++ Module/Panel/IP-info/Moore/IP-info.js
@@ -9,7 +9,7 @@ $httpClient.get(url, function(error, response, data){
     let isp = jsonData.isp
     let ip = jsonData.query
 // 避免City与Country重复出现
-let location = (country === city) ? `${emoji} ${country}` : `${emoji}${countryCode}-${city}`;
+let location = (country === city) ? `${emoji}${country}` : `${emoji}${countryCode}-${city}`;
 // 去除 isp 变量中的标点符号和 "Communications" 等过长的无意义词语(不区分大小写）
 let cleanedIsp = isp.replace(/[,]|(\.$)|\([^)]*\)|\-|DinServer|Communications|Trading|as|Telekomunikasyon|Hosted|Information|Technology|Technologies|Television|Registration/gi, '');
 // 将运营商信息中的连续两个或多个空格替换为一个空格
