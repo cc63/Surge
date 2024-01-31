1dc0eb87d6a471a527b5b2be24f45c16913b724b
cc63 <99179360+cc63@users.noreply.github.com>
2024-01-31 13:42:30 +0800

    更新 IP-info.js

--- Module/Panel/IP-info/Moore/IP-info.js
+++ Module/Panel/IP-info/Moore/IP-info.js
@@ -16,9 +16,9 @@ $httpClient.get(url, function(error, response, data){
 // 避免City与Country重复
 let location = (country === city) ? `${emoji}${country}` : `${emoji}${countryCode} │ ${city}`;
 // 让ISP信息优雅的呈现在一行内
-let cleanedIsp = isp.replace(/[,]| \-|\.$|\(.*\)|\b(Hong Kong|Mass internet|Communications?|information|Technolog(y|ies)|Chunghwa|Taiwan)\b|munications?/gi, '');
+let cleanedIsp = isp.replace(/([,]|\-|\.$|\(.*\)|\b(Hong Kong|Mass internet|Communications?|information|Technolog(y|ies)|Chunghwa|Taiwan)\b|munications?)\s?/gi, '');
 // 去除可能出现的连续空格
-    cleanedIsp = cleanedIsp.replace(/ {2,}/g, ' ');    
+//cleanedIsp = cleanedIsp.replace(/ {2,}/g, ' ');    
 // 然后将 cleanedIsp 用于通知内容
 let body = {
     title: "节点信息",
