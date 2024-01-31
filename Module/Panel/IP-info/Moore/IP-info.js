6071a4a1bb6f3a6dccd96737f16bdeec9b0f4596
cc63 <99179360+cc63@users.noreply.github.com>
2024-01-31 21:39:06 +0800

    更新 IP-info.js

--- Module/Panel/IP-info/Moore/IP-info.js
+++ Module/Panel/IP-info/Moore/IP-info.js
@@ -22,7 +22,7 @@ let body = {
     title: "节点信息",
     content: `IP地址：${ip}\n运营商：${cleanedIsp}\n所在地：${location}`,
     icon: "globe.asia.australia",
-   'icon-color': '#6FD087'
+   'icon-color': '#70B581'
 }
     $done(body);
 });
