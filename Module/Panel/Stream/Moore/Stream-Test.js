1f33a9a22c1935d5de8cb8f7015d9f35939a5908
cc63 <99179360+cc63@users.noreply.github.com>
2024-02-03 14:58:20 +0800

    更新 Stream-Test.js

--- Module/Panel/Stream/Moore/Stream-Test.js
+++ Module/Panel/Stream/Moore/Stream-Test.js
@@ -24,7 +24,7 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
 
 ;(async () => {
   let panel_result = {
-    title: '流媒体解锁检测',
+    title: '流媒体解锁',
     content: '',
     icon: 'play.tv.fill',
     'icon-color': '#DC3131',
