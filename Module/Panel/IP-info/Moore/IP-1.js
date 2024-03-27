4deb6504807a76584897df56669c72e7cc1bcd20
cc63 <99179360+cc63@users.noreply.github.com>
2024-03-27 10:36:35 +0800

    优化代码

--- Module/Panel/IP-info/Moore/IP-1.js
+++ Module/Panel/IP-info/Moore/IP-1.js
@@ -1,41 +1,46 @@
 /**********
 * 作者：cc63&ChatGPT
-* 更新时间：2024年2月1日
+* 更新时间：2024年3月27日
 **********/
 
-let url = "http://ip-api.com/json"
+const url = "http://ip-api.com/json";
 
-$httpClient.get(url, function(error, response, data){
-    let jsonData = JSON.parse(data)
-    let country = jsonData.country
-    let countryCode = jsonData.countryCode
-    let emoji = getFlagEmoji(jsonData.countryCode)
-    let city = jsonData.city
-    let isp = jsonData.isp
-    let ip = jsonData.query
-// 避免City与Country重复
-let location = (country === city) ? `${emoji} │ ${country}` : `${emoji}${countryCode} │ ${city}`;
-// 去除ISP信息中的无意义信息
-let cleanedIsp = isp.replace(/\s?[,]|\s\-|\.$|\(.*\)|(\b(Hong Kong|Mass internet|Communications?|information|Technolog(y|ies)|ESolutions?|Services Limited)\b)\s?|munications?/gi, '');    
-// 避免可能出现的连续空格
-cleanedIsp = cleanedIsp.replace(/\s+/g, ' ');    
-// 然后将 cleanedIsp 用于通知内容
-let body = {
-    title: "节点信息",
-    content: `IP地址：${ip}\n运营商：${cleanedIsp}\n所在地：${location}`,
-    icon: "globe.asia.australia",
-   'icon-color': '#3D90ED'
-}
-    $done(body);
+$httpClient.get(url, (error, response, data) => {
+    if (error) {
+        console.error('请求错误：', error);
+        return $done();
+    }
+
+    try {
+        const jsonData = JSON.parse(data);
+        const { country, countryCode, city, isp, query: ip } = jsonData;
+        const emoji = getFlagEmoji(countryCode);
+        const location = (country === city) ? `${emoji} │ ${country}` : `${emoji} ${countryCode} │ ${city}`;
+        const cleanedIsp = cleanIspInfo(isp);
+
+        const body = {
+            title: "节点信息",
+            content: `IP地址：${ip}\n运营商：${cleanedIsp}\n所在地：${location}`,
+            icon: "globe.asia.australia",
+            'icon-color': '#3D90ED'
+        };
+
+        $done(body);
+    } catch (e) {
+        console.error('解析错误：', e);
+        $done();
+    }
 });
 
 function getFlagEmoji(countryCode) {
-    if (countryCode.toUpperCase() == 'TW') {
-        countryCode = 'CN'
+    // 特殊处理台湾的国旗情况
+    if (countryCode.toUpperCase() === 'TW') {
+        countryCode = 'CN'; // 或根据需要将'CN'替换为其他代表台湾的字符或表情符号
     }
-    const codePoints = countryCode
-        .toUpperCase()
-        .split('')
-        .map(char => 127397 + char.charCodeAt())
-    return String.fromCodePoint(...codePoints)
+    return String.fromCodePoint(...countryCode.toUpperCase().split('').map(char => 127397 + char.charCodeAt()));
+}
+
+function cleanIspInfo(isp) {
+    return isp.replace(/\s?[,]|\s\-|\.$|\(.*\)|(\b(Hong Kong|Mass internet|Communications?|information|Technolog(y|ies)|ESolutions?|Services Limited)\b)\s?|munications?/gi, '')
+              .replace(/\s+/g, ' ');
 }
